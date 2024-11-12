23ade0a7798f2b260a23bc5ff2e60109b7976bca
sserges <agounkeserge@gmail.com>
2024-11-12 11:44:47 -0700

    first contract

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -2,17 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract Counter {
-    uint count;
-
-    constructor() {
-        count = 0;
-    }
-
-    function getCount() public view returns(uint) {
-        return count;
-    }
+    uint public count = 0;
 
     function incrementCount() public {
-        count = count + 1;
+        count++;
     }
 }
