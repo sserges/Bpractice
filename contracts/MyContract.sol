718a477dce463cf650f3705cfba20a0c79bb0549
sserges <agounkeserge@gmail.com>
2024-11-12 23:21:02 -0700

    Arrays

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -2,25 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    // State Variables
-    uint public myUint  = 1;
-    uint256 public myUint256 = 1 ;
+    // Arrays
+    uint[] public uintArray = [1, 2, 3];
+    string[] public stringArray = ["apple", "banana", "carrot"];
+    string[] public values;
+    uint256[][] public array2D = [[1, 2, 3], [4, 5, 6]];
 
-    string public myString = "Hello, world!";
-
-    address public myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-
-    struct MyStruct {
-        uint256 myUint256;
-        string myStrimg;
+    function addValue(string memory _value) public {
+        values.push(_value);
     }
 
-    MyStruct public myStruct = MyStruct(1, "Hello");
-
-    // Local Variables
-    function getValue() public pure returns(uint) {
-        uint value = 1;
-
-        return value;
+    function valueCount() public view returns(uint) {
+        return values.length;
     }
+
+
 }
