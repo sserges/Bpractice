7151190932c3960811e1021edbb2380539f9af97
sserges <agounkeserge@gmail.com>
2024-11-18 10:17:46 -0700

    Full Contract

---
+++ contracts/HotelRoom.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HotelRoom {
+    // Vacant
+    // Occupied
+
+    enum Statuses {
+        Vacant,
+        Occupied
+    }
+
+    Statuses public currentStatus;
+
+    event Occupy(address _occupant, uint _value);
+
+    address payable public owner;
+
+    constructor() {
+        owner = payable(msg.sender);
+        currentStatus = Statuses.Vacant;
+    }
+
+    modifier onlyWhileVacant {
+        require(currentStatus == Statuses.Vacant, "Currently occupied");
+        _;
+    }
+
+    modifier costs(uint _amount) {
+        require(msg.value >= _amount, "Not enough ether provider");
+        _;
+    }
+
+
+    function book() public payable onlyWhileVacant costs(2 ether) {
+        currentStatus = Statuses.Occupied;
+
+        // owner.transfer(msg.value);
+
+        (bool sent, bytes memory data) = owner.call{value: msg.value}("");
+        require(true);
+
+        emit Occupy(msg.sender, msg.value);
+    }
+}
